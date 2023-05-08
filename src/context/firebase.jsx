cec81ea2d205a7aa85b9caed64a1339b6d31ee37
Baacha Adithya <85406431+adithyathecoder@users.noreply.github.com>
2023-05-09 00:00:04 +0530

    Update firebase.jsx

--- src/context/firebase.jsx
+++ src/context/firebase.jsx
@@ -17,14 +17,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyADTaH11XtXtGukJycLHCTMvA7Voe6wONo",
-  authDomain: "artefact-afd9c.firebaseapp.com",
-  projectId: "artefact-afd9c",
-  storageBucket: "artefact-afd9c.appspot.com",
-  messagingSenderId: "835758993180",
-  appId: "1:835758993180:web:3cf54c9b3d17fdbf9d94d9",
-  measurementId: "G-MJNM422M8E",
-  databaseURL : "https://artefact-afd9c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    apiKey: "AIzaSyAk0pRlxHeKNCWw8_7PH3pJQv64n7_kE9I",
+    authDomain: "treasure-hunt-elitmus.firebaseapp.com",
+    projectId: "treasure-hunt-elitmus",
+    storageBucket: "treasure-hunt-elitmus.appspot.com",
+    messagingSenderId: "1036579027692",
+    appId: "1:1036579027692:web:5265ad9a63b21485db03c9",
+    measurementId: "G-Y07VSHEV90",
+    databaseURL : "https://treasure-hunt-elitmus-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
